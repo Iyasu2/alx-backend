30179bdae50101d85f46031d3200170f4c2585c3
iyasu2 <iyasuforreal@gmail.com>
2024-01-31 19:08:40 +0300

    s

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+this is a module
+'''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache inherits from BaseCaching
+    """
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value for the key key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value in self.cache_data linked to key
+        """
+        return self.cache_data.get(key) if key is not None else None
