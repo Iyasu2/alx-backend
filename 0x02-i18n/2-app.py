988314a3db009e23bd0c7189d19877fb55aef8ea
iyasu2 <iyasuforreal@gmail.com>
2024-02-06 20:27:54 +0300

    s

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''
+this is a module
+'''
+from flask import Flask, render_template
+from flask_babel import Babel, gettext
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    '''
+    use the native language if it's in the list of supported languages
+    '''
+    return request.accept_languages.best_match(['en', 'fr', 'es', 'de'])
+
+
+@app.route("/")
+def index():
+    '''
+    this is the index route
+    '''
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
